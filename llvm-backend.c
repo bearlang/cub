cd8dc624cc4cbd4d7aee03598f5aee3d43be4e84
Colby Skeggs <git@colbyskeggs.com>
2015-07-25 13:48:47 -0700

    Switched llvm-backend to a patch-based system.

--- llvm-backend.c
+++ llvm-backend.c
@@ -1,10 +1,129 @@
+#define _GNU_SOURCE
+#include <stdio.h>
 #include <string.h>
 
 #include "backend.h"
 
-#define pf(...) fprintf(out, __VA_ARGS__);
+struct patchent {
+	bool is_ref;
+	union {
+		char *data;
+		struct patchvar *ref;
+	};
+	struct patchent *next;
+};
 
-static void wt(FILE *out, type *t) {
+struct patchvar {
+	char *value;
+	struct patchvar *next;
+};
+
+struct patchent *fent = NULL;
+struct patchvar *fvar = NULL;
+
+void pt_reset() {
+	{
+		struct patchent *cur = fent;
+		while (cur != NULL) {
+			struct patchent *next = cur->next;
+			if (!cur->is_ref) {
+				free(cur->data);
+			}
+			free(cur);
+			cur = next;
+		}
+	}
+	{
+		struct patchvar *cur = fvar;
+		while (cur != NULL) {
+			struct patchvar *next = cur->next;
+			free(cur->value);
+			free(cur);
+			cur = next;
+		}
+	}
+	fent = NULL;
+	fvar = NULL;
+}
+
+void pt_finalize(FILE *out) {
+	struct patchent *cur = fent;
+	struct patchent *last = NULL;
+
+	while (cur != NULL) { // reverse the linked list
+		struct patchent *next = cur->next;
+		cur->next = last;
+		last = cur;
+		cur = next;
+	}
+	fent = last;
+
+	cur = fent;
+	while (cur != NULL) {
+		if (cur->is_ref) {
+			if (cur->ref->value == NULL) {
+				fputs("variable not completed\n", stderr);
+				exit(1);
+			}
+			fputs(cur->ref->value, out);
+		} else {
+			fputs(cur->data, out);
+		}
+		cur = cur->next;
+	}
+
+	pt_reset();
+}
+
+struct patchvar *pt_def() {
+	struct patchvar *out = malloc(sizeof(struct patchvar));
+	out->next = fvar;
+	fvar = out;
+	out->value = NULL;
+	return out;
+}
+
+void pt_put(struct patchvar *var, char *value) {
+	if (value == NULL) {
+		fputs("value is NULL\n", stderr);
+		exit(1);
+	}
+	if (var->value != NULL) {
+		free(var->value);
+	}
+	var->value = value;
+}
+
+const char *pt_fetch(struct patchvar *var) {
+	if (var->value == NULL) {
+		fputs("fetch on NULL var\n", stderr);
+		exit(1);
+	}
+	return var->value;
+}
+
+struct patchent *pt_new_ent(bool is_ref) {
+	struct patchent *out = malloc(sizeof(struct patchent));
+	out->next = fent;
+	fent = out;
+	out->is_ref = is_ref;
+	return out;
+}
+
+void pt_use(struct patchvar *var) {
+	pt_new_ent(true)->ref = var;
+}
+
+void pt_write(char *text) {
+	pt_new_ent(false)->data = text;
+}
+
+#define pf(...) { char *cptr; if (asprintf(&cptr, __VA_ARGS__) == -1) { perror("asprintf"); exit(1); } pt_write(cptr); }
+#define pv(var) { pt_use(var); }
+#define vf(var, ...) { char *cptr; if (asprintf(&cptr, __VA_ARGS__) == -1) { perror("asprintf"); exit(1); } pt_put(var, cptr); }
+#define vfr(var, x) { pt_put(var, x); }
+
+static void wt(type *t) {
 	if (t == NULL) {
 		abort();
 	}
@@ -16,11 +135,11 @@ static void wt(FILE *out, type *t) {
 		pf("void (");
 		argument *arg = t->blocktype;
 		if (arg != NULL) {
-			wt(out, arg->argument_type);
+			wt(arg->argument_type);
 			arg = arg->next;
 			for (; arg != NULL; arg = arg->next) {
 				pf(", ");
-				wt(out, arg->argument_type);
+				wt(arg->argument_type);
 			}
 		}
 		pf(")*");
@@ -70,6 +189,8 @@ static void wt(FILE *out, type *t) {
 }
 
 void backend_write(code_system *system, FILE *out) {
+	pt_reset();
+
 	pf("target datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\"\n"
 		 "target triple = \"x86_64-unknown-linux-gnu\"\n\n\n");
 
@@ -82,7 +203,7 @@ void backend_write(code_system *system, FILE *out) {
 				if (j != 0) {
 					pf(", ");
 				}
-				wt(out, str->fields[j].field_type);
+				wt(str->fields[j].field_type);
 			}
 			pf(" };\n");
 		}
@@ -104,11 +225,11 @@ void backend_write(code_system *system, FILE *out) {
 
 		size_t offset = block->parameter_count;
 		if (offset) {
-			wt(out, block->parameters[0].field_type);
+			wt(block->parameters[0].field_type);
 
 			for (size_t j = 1; j < offset; j++) {
 				pf(", ");
-				wt(out, block->parameters[j].field_type);
+				wt(block->parameters[j].field_type);
 			}
 		}
 
@@ -116,16 +237,15 @@ void backend_write(code_system *system, FILE *out) {
 
 #define TYPEOF(x) ((x) < offset ? block->parameters[x].field_type : block->instructions[x - offset].type)
 
-		const char *ref[block->instruction_count + offset];
-		bool alloced[block->instruction_count + offset];
-#define RP(n) (ref[ins->parameters[(n)]])
+		struct patchvar *ref[block->instruction_count + offset];
+
+#define RP(n) pt_fetch(ref[ins->parameters[(n)]])
+#define PR(n) pv(ref[ins->parameters[(n)]]);
 #define TP(n) (TYPEOF(ins->parameters[(n)]))
 
 		for (size_t k = 0; k < offset + block->instruction_count; k++) {
-			char *tmp = malloc(64);
-			snprintf(tmp, 64, k < offset ? "%%%zu" : "%%n%zu", k);
-			ref[k] = tmp;
-			alloced[k] = true;
+			ref[k] = pt_def();
+			vf(ref[k], k < offset ? "%%%zu" : "%%n%zu", k);
 		}
 
 		for (size_t j = 0; j < block->instruction_count; j++) {
@@ -135,18 +255,12 @@ void backend_write(code_system *system, FILE *out) {
 			switch (ins->operation.type) {
 			case O_BITWISE_NOT:
 				pf("  %%n%zu = xor ", k);
-				wt(out, ins->type);
+				wt(ins->type);
 				pf(" %s, -1", RP(0));
 				break;
-			case O_BLOCKREF: {
-				if (alloced[k]) {
-					free((char*) ref[k]);
-				}
-				char *tmp = malloc(64);
-				snprintf(tmp, 64, "@block_%zu", ins->block_index);
-				ref[k] = tmp;
-				alloced[k] = true;
-			} break;
+			case O_BLOCKREF:
+				vf(ref[k], "@block_%zu", ins->block_index);
+				break;
 			case O_CAST: {
 				type *typ = ins->type;
 				const char *name = NULL;
@@ -189,9 +303,9 @@ void backend_write(code_system *system, FILE *out) {
 					break;
 				}
 				pf("  %%n%zu = %s ", k, name);
-				wt(out, TP(np));
+				wt(TP(np));
 				pf(" %s to ", RP(np));
-				wt(out, typ);
+				wt(typ);
 			} break;
 			case O_COMPARE: {
 				const char *op;
@@ -204,29 +318,29 @@ void backend_write(code_system *system, FILE *out) {
 				case O_NE: op = "icmp ne"; break;
 				}
 				pf("  %%n%zu = %s ", k, op);
-				wt(out, TP(0));
+				wt(TP(0));
 				pf(" %s, %s", RP(0), RP(1));
 			} break;
 			case O_GET_FIELD:
 				pf("  %%temp.%zu = getelementptr inbounds ", k);
-//				wt(out, TP(0));
+//				wt(TP(0));
 //				pf(", ");
-				wt(out, TP(0));
+				wt(TP(0));
 				pf(" %s, i64 0, i32 %zu\n", RP(0), ins->parameters[1]);
 
 				pf("  %%n%zu = load ", k);
-				wt(out, ins->type);
+				wt(ins->type);
 				pf("* %%temp.%zu, align 1", k);
 				break;
 			case O_GET_INDEX:
 				pf("  %%temp.%zu = getelementptr inbounds ", k);
-//				wt(out, TP(0));
+//				wt(TP(0));
 //				pf(", ");
-				wt(out, TP(0));
+				wt(TP(0));
 				pf(" %s, i64 %%n%zu\n", RP(0), ins->parameters[1]);
 
 				pf("  %%n%zu = load ", k);
-				wt(out, ins->type);
+				wt(ins->type);
 				pf("* %%temp.%zu, align 1", k);
 				break;
 			case O_GET_SYMBOL:
@@ -240,9 +354,6 @@ void backend_write(code_system *system, FILE *out) {
 				fprintf(stderr, "this backend does not support instanceof checking\n");
 				exit(1);
 			case O_LITERAL:
-				if (alloced[k]) {
-					free((char*) ref[k]);
-				}
 				switch (ins->type->type) {
 				case T_ARRAY:
 				case T_BLOCKREF:
@@ -258,17 +369,15 @@ void backend_write(code_system *system, FILE *out) {
 				default:
 					abort();
 				case T_BOOL:
-					ref[k] = (ins->value_bool ? "true" : "false");
-					alloced[k] = false;
+					vf(ref[k], ins->value_bool ? "true" : "false");
 					break;
 				case T_OBJECT:
-					ref[k] = "null";
-					alloced[k] = false;
+					vf(ref[k], "null");
 					break;
 				case T_STRING: {
 					char *tmp = malloc(strlen(ins->value_string) * 3 + 4);
 					strcpy(tmp, "c\"");
-					int ti = strlen(tmp);
+					size_t ti = strlen(tmp);
 					for (char *s = ins->value_string; *s; s++) {
 						char c = *s;
 						if (c >= 32 && c <= 126 && c != '"' && c != '\\') {
@@ -279,42 +388,29 @@ void backend_write(code_system *system, FILE *out) {
 						}
 					}
 					strcat(tmp, "\"");
-					ref[k] = tmp;
-					alloced[k] = true;
+					vfr(ref[k], tmp);
 				} break;
-				case T_U8: {
-					char *tmp = malloc(32);
-					snprintf(tmp, 32, "%hhu", ins->value_u8);
-					ref[k] = tmp;
-					alloced[k] = true;
-				} break;
-				case T_U16: {
-					char *tmp = malloc(32);
-					snprintf(tmp, 32, "%hu", ins->value_u16);
-					ref[k] = tmp;
-					alloced[k] = true;
-				} break;
-				case T_U32: {
-					char *tmp = malloc(32);
-					snprintf(tmp, 32, "%u", ins->value_u32);
-					ref[k] = tmp;
-					alloced[k] = true;
-				} break;
-				case T_U64: {
-					char *tmp = malloc(32);
-					snprintf(tmp, 32, "%lu", ins->value_u64);
-					ref[k] = tmp;
-					alloced[k] = true;
-				} break;
+				case T_U8:
+					vf(ref[k], "%hhu", ins->value_u8);
+					break;
+				case T_U16:
+					vf(ref[k], "%hu", ins->value_u16);
+					break;
+				case T_U32:
+					vf(ref[k], "%u", ins->value_u32);
+					break;
+				case T_U64:
+					vf(ref[k], "%lu", ins->value_u64);
+					break;
 				}
 				break;
 			case O_LOGIC: {
 				pf("  %%a.%zu = icmp ne ", k);
-				wt(out, TP(0));
+				wt(TP(0));
 				pf(" %s, 0\n", RP(0));
 
 				pf("  %%b.%zu = icmp ne ", k);
-				wt(out, TP(1));
+				wt(TP(1));
 				pf(" %s, 0\n", RP(1));
 
 				pf("  %%n%zu = ", k);
@@ -335,7 +431,7 @@ void backend_write(code_system *system, FILE *out) {
 			} break;
 			case O_NEGATE:
 				pf("  %%n%zu = sub ", k);
-				wt(out, ins->type);
+				wt(ins->type);
 				pf(" 0, %s", RP(0));
 				break;
 			case O_NEW:
@@ -347,7 +443,7 @@ void backend_write(code_system *system, FILE *out) {
 				break;
 			case O_NOT:
 				pf("  %%n%zu = icmp eq ", k);
-				wt(out, TP(0));
+				wt(TP(0));
 				pf(" %s, 0", RP(0));
 				break;
 			case O_NUMERIC: {
@@ -364,7 +460,7 @@ void backend_write(code_system *system, FILE *out) {
 				default: abort();
 				}
 				pf("  %%n%zu = %s ", k, op);
-				wt(out, ins->type);
+				wt(ins->type);
 				pf(" %s, %s", RP(0), RP(1));
 			} break;
 			case O_SET_FIELD:
@@ -374,28 +470,28 @@ void backend_write(code_system *system, FILE *out) {
 					fprintf(stderr, "expected object in SET_FIELD");
 					abort();
 				}
-				wt(out, t);
+				wt(t);
 				pf(" %s, i64 0, i32 %zu\n", RP(0), ins->parameters[1]);
 
 				type *ft = system->structs[t->struct_index].fields[ins->parameters[1]].field_type;
 
 				pf("  store ");
-				wt(out, ft);
+				wt(ft);
 				pf(" %s, ", RP(2));
-				wt(out, ft);
+				wt(ft);
 				pf("* %%temp.%zu, align 1", k);
 				break;
 			case O_SET_INDEX:
 				pf("  %%temp.%zu = getelementptr inbounds ", k);
-				wt(out, TP(0));
+				wt(TP(0));
 				pf(" %s, i64 %%n%zu\n", RP(0), ins->parameters[1]);
 
 				type *et = t->arraytype;
 
 				pf("  store ");
-				wt(out, et);
+				wt(et);
 				pf(" %s, ", RP(2));
-				wt(out, et);
+				wt(et);
 				pf("* %%temp.%zu, align 1", k);
 				break;
 			case O_SHIFT: {
@@ -414,7 +510,7 @@ void backend_write(code_system *system, FILE *out) {
 					abort();
 				}
 				pf("  %%n%zu = %s ", k, op);
-				wt(out, ins->type);
+				wt(ins->type);
 				pf(" %s, %s", RP(0), RP(1));
 			} break;
 			case O_STR_CONCAT:
@@ -438,48 +534,48 @@ void backend_write(code_system *system, FILE *out) {
 		} else {
 			switch (block->tail.type) {
 			case GOTO: {
-				pf("  tail call fastcc void %s(", ref[block->tail.first_block]);
+				pf("  tail call fastcc void %s(", pt_fetch(ref[block->tail.first_block]));
 				size_t params = block->tail.parameter_count;
 				if (params) {
-					wt(out, TYPEOF(block->tail.parameters[0]));
-					pf(" %s", ref[block->tail.parameters[0]]);
+					wt(TYPEOF(block->tail.parameters[0]));
+					pf(" %s", pt_fetch(ref[block->tail.parameters[0]]));
 
 					for (size_t i = 1; i < params; i++) {
 						pf(", ");
-						wt(out, TYPEOF(block->tail.parameters[i]));
-						pf(" %s", ref[block->tail.parameters[i]]);
+						wt(TYPEOF(block->tail.parameters[i]));
+						pf(" %s", pt_fetch(ref[block->tail.parameters[i]]));
 					}
 				}
 				pf(") noreturn\n");
 			} break;
 			case BRANCH: {
-				pf("  br i1 %s, label %%iftrue, label %%iffalse\n", ref[block->tail.condition]);
+				pf("  br i1 %s, label %%iftrue, label %%iffalse\n", pt_fetch(ref[block->tail.condition]));
 				pf("iftrue:\n");
-				pf("  tail call fastcc void %s(", ref[block->tail.first_block]);
+				pf("  tail call fastcc void %s(", pt_fetch(ref[block->tail.first_block]));
 				size_t params = block->tail.parameter_count; // TODO: less code duplication
 				if (params) {
-					wt(out, TYPEOF(block->tail.parameters[0]));
-					pf(" %s", ref[block->tail.parameters[0]]);
+					wt(TYPEOF(block->tail.parameters[0]));
+					pf(" %s", pt_fetch(ref[block->tail.parameters[0]]));
 
 					for (size_t i = 1; i < params; i++) {
 						pf(", ");
-						wt(out, TYPEOF(block->tail.parameters[i]));
-						pf(" %s", ref[block->tail.parameters[i]]);
+						wt(TYPEOF(block->tail.parameters[i]));
+						pf(" %s", pt_fetch(ref[block->tail.parameters[i]]));
 					}
 				}
 				pf(") noreturn\n");
 				pf("  ret void\n");
 
 				pf("iffalse:\n");
-				pf("  tail call fastcc void %s(", ref[block->tail.second_block]);
+				pf("  tail call fastcc void %s(", pt_fetch(ref[block->tail.second_block]));
 				if (params) {
-					wt(out, TYPEOF(block->tail.parameters[0]));
-					pf(" %s", ref[block->tail.parameters[0]]);
+					wt(TYPEOF(block->tail.parameters[0]));
+					pf(" %s", pt_fetch(ref[block->tail.parameters[0]]));
 
 					for (size_t i = 1; i < params; i++) {
 						pf(", ");
-						wt(out, TYPEOF(block->tail.parameters[i]));
-						pf(" %s", ref[block->tail.parameters[i]]);
+						wt(TYPEOF(block->tail.parameters[i]));
+						pf(" %s", pt_fetch(ref[block->tail.parameters[i]]));
 					}
 				}
 				pf(") noreturn\n");
@@ -489,15 +585,10 @@ void backend_write(code_system *system, FILE *out) {
 		}
 
 		pf("  ret void\n}\n");
-
-		for (size_t k = 0; k < offset + block->instruction_count; k++) {
-			if (alloced[k]) {
-				free((char*) ref[k]);
-			}
-			ref[k] = NULL;
-		}
 	}
 
 	pf("\n\n");
 	pf("attributes #0 = { inlinehint noreturn nounwind }\n");
+
+	pt_finalize(out);
 }
