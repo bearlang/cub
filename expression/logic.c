e03de48408bc17f8879fa5f6f1d57adba29bf5cd
Eli Skeggs <skeggse@gmail.com>
2015-07-26 11:35:59 -0700

    Fix logic expression constructor variable naming
    Signed-off-by: Eli Skeggs <skeggse@gmail.com>

--- expression/logic.c
+++ expression/logic.c
@@ -1,10 +1,11 @@
 #include "../xalloc.h"
 #include "../expression.h"
 
-expression *new_logic_node(logic_type type, expression *left,
+expression *new_logic_node(logic_type ltype, expression *left,
     expression *right) {
   expression *logic = xmalloc(sizeof(*logic));
   logic->operation.type = O_LOGIC;
+  logic->operation.logic_type = ltype;
   logic->type = xmalloc(sizeof(type));
   logic->type->type = T_BOOL;
   logic->value = left;
