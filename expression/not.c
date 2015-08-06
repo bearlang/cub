a3aabbb6ded3a1c3708107a8146a05a86aeabc33
Eli Skeggs <skeggse@gmail.com>
2015-08-05 20:19:48 -0700

    Fix not expression type defaults
    Signed-off-by: Eli Skeggs <skeggse@gmail.com>

--- expression/not.c
+++ expression/not.c
@@ -4,8 +4,9 @@
 expression *new_not_node(bool bitwise, expression *value) {
   expression *not = xmalloc(sizeof(*not));
   not->operation.type = bitwise ? O_BITWISE_NOT : O_NOT;
-  not->type = xmalloc(sizeof(type));
-  not->type->type = T_BOOL;
+  if (bitwise) {
+    not->type = new_type(T_BOOL);
+  }
   not->value = value;
   not->next = NULL;
   return not;
